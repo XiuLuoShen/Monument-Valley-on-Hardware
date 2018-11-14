40f8d4307d6ec8d0d695c9c7a6ff23ff59464daa
songeunyou <songeunyou@users.noreply.github.com>
2018-11-14 15:10:01 -0500

    Added new movement to v2 file

--- movementLogic_v2.v
+++ movementLogic_v2.v
@@ -139,8 +139,8 @@ module moveSpriteDataPath(
 
   always @(posedge clock) begin
     if (!resetn) begin
-      X <= 9'd1;  // initial sprite location
-      Y <= 8'd16;
+      X <= 8'd96;  // initial sprite location
+      Y <= 9'd222;
       validMove <= 1'b0;
     end
 
@@ -149,44 +149,44 @@ module moveSpriteDataPath(
         if (newX <= 1'b0 || newY <= 1'b0)
           validMove = 1'b0; // ensures the square does not go off screen
 
-      	// starting point to first door
-        else if (newY == 9'd222 - newX) // diagonal BL to TR
+        // starting point to first door
+        else if (newY == 9'd318 - newX) // diagonal BL to TR
           if (newX <= 8'd122 && newX >= 8'd96)
             validMove = 1'b1;
 
         // door to first corner
-        else if (newY == 8'd96 + newX) // diagonal TL to BR
+        else if (newY == newX - 8'd31) // diagonal TL to BR
           if (newX >= 8'd127 && newX <= 8'd181)
             validMove = 1'b1;
 
         // first corner to first button
-        else if (newY == 9'd113 - newX)
+        else if (newY == 8'd238 - newX)
           if (newX >= 8'd125 && newX <= 8'd181)
             validMove = 1'b1;
 
         // first button to moving platform
-        else if (newY == 9'd159 - newX)
+        else if (newY == 8'd284 - newX)
           if (newX >= 8'd125 && newX <= 8'd161)
             validMove = 1'b1;
 
         // moving platform to island
-        else if (newY == 9'd123 + newX)
+        else if (newY == newX - 8'd38)
           if (newX >= 8'd161 && newX <= 8'd216)
             validMove = 1'b1;
 
         // island
-        else if (newY == 9'd178 - newX)
+        else if (newY == 8'd394 - newX)
           if (newX >= 8'd180 && newX <= 8'd216)
             validMove = 1'b1;
 
         // island to first button again
-        else if (newY == 9'd214 + newX)
+        else if (newY == newX - 8'd89)
           if (newX >= 8'd125 && newX <= 8'd180)
             validMove = 1'b1;
 
         // top of platform to end
-        else if (newY == 9'd187 - newX)
-          if (newX >= 8'd159 && newX <= 8'd127)
+        else if (newY == 8'd212 - newX)
+          if (newX >= 8'd159 && newX <= 8'd125)
             validMove = 1'b1;
 
         else validMove = 1'b0;
